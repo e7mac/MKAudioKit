5811eade94f6f7a7d79639192510568ce5d51e9e
e7mac <mayank.ot@gmail.com>
2015-07-18 17:43:10 -0700

    add preset code

---
+++ Classes/MKPresetManagerViewControllerCell.h
@@ -0,0 +1,16 @@
+//
+//  MKPresetManagerViewControllerCell.h
+//  bent.fm
+//
+//  Created by Mayank Sanganeria on 1/31/15.
+//  Copyright (c) 2015 Mayank Sanganeria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKPresetManagerViewControllerCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *patchName;
+@property (weak, nonatomic) IBOutlet UILabel *patchAuthor;
+
+@end
---
+++ Classes/MKPresetManagerViewControllerCell.m
@@ -0,0 +1,17 @@
+//
+//  MKPresetManagerViewControllerCell.m
+//  bent.fm
+//
+//  Created by Mayank Sanganeria on 1/31/15.
+//  Copyright (c) 2015 Mayank Sanganeria. All rights reserved.
+//
+
+#import "MKPresetManagerViewControllerCell.h"
+
+@implementation MKPresetManagerViewControllerCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
